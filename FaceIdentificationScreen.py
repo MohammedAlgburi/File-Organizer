4cba61bfaebf4ab60be4733b93f51b484b284b9b
Mohammed <alg.mohammedpro@gmail.com>
2025-07-15 22:54:06 -0700

    Got started on the Face identificaiton screen and got the no picture image to show up on the app!

--- FaceIdentificationScreen.py
+++ FaceIdentificationScreen.py
@@ -1,19 +1,48 @@
-from PySide6.QtWidgets import QWidget, QHBoxLayout, QLabel, QVBoxLayout
-from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QWidget, QHBoxLayout, QLabel, QVBoxLayout, QStackedWidget
+from PySide6.QtGui import QPixmap, QDrag
+import resources_rc as rc
 
 class FaceIdentificationScreen(QWidget):
     def __init__(self):
         super().__init__()
-        self.main_layout = QHBoxLayout()
+        # THIS MIGHT BE A LOCAL VARIABLE BUT IM PUTTING IT HERE FOR NOW
+        self.image_label = QLabel()
 
-    def run(self):
-        pass
+    def setup_ui(self):
+        main_layout = QHBoxLayout()
+        main_layout.addWidget(self.picture_display())        
 
-    def picture_display(self):
-        picture_box = QLabel()
+        self.setLayout(main_layout)
+
+    def picture_display(self) -> QStackedWidget:
+        # This widget controls the different states of this box.
+        # This box has three states: 
+        # 1. no-picture-selected, 
+        # 2. drag and drop, 
+        # 3. image (only one which consistently changes)
+        
+        picture_display_manager = QStackedWidget()
+
+        no_picture_label = QLabel(pixmap=QPixmap(":/rc/Resources/Picture-Select-image.png"))
+        # TODO: Make the resource for the drag and drop.
+        # TODO: Create the drag and drop functionality
+        drag_and_drop_label = QLabel(pixmap=QPixmap(""))
+
+        picture_display_manager.addWidget(no_picture_label)
+        picture_display_manager.addWidget(drag_and_drop_label)
+        picture_display_manager.addWidget(self.image_label)
+
+        picture_display_manager.setCurrentIndex(0)
+
+        return picture_display_manager
+
+
+    def refresh_image_label(self, image_path: str):
+        self.image_label.clear()
+        self.image_label.setPixmap(QPixmap(image_path))
 
     def people_panel(self):
+        panel = QWidget()
+
+    def create_name_label(self):
         pass
-    
-class DragAndDropBox():
-    pass
--- MainWindow.py
+++ MainWindow.py
@@ -1,6 +1,7 @@
 import sys
 from PySide6.QtWidgets import QMainWindow, QWidget, QApplication, QStackedWidget
 from PySide6 import QtWidgets
+from FaceIdentificationScreen import FaceIdentificationScreen
 
 
 def run_gui():
@@ -17,3 +18,17 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("File Organizer")
         self.resize(800, 600)
         self.page_manager = QStackedWidget()
+        self.setCentralWidget(self.page_manager)
+
+        self.setup_ui()
+        
+    def setup_ui(self):
+        # THIS PART MIGHT GO GLOBAL IF I EVER NEED TO ACCESS IT LATER
+        face_identification_screen = FaceIdentificationScreen()
+        face_identification_screen.setup_ui()
+        
+        self.page_manager.addWidget(face_identification_screen)
+        self.page_manager.setCurrentIndex(0)
+
+if __name__ == "__main__":
+    pass
